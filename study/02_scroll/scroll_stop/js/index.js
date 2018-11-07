917853d986efef439966c29e65de5ebba5117d1b
kei5693 <keichi5693@naver.com>
2018-11-08 06:41:57 +0900

    test script

---
+++ study/02_scroll/scroll_stop/js/index.js
@@ -0,0 +1,23 @@
+﻿$(function(){
+	// quick scroll
+	
+	var scrollSet = $("#quick").offset().top,
+		fixedPos = $('#container').offset().top,
+		calPos = ($("#wrap").height() + $("#header").height()+10) - ($("#quick").height() + $("#footer").height()+10);
+	
+
+	$(window).scroll(function(){
+		var scrollTop = $(document).scrollTop();
+		console.log(scrollTop);
+				
+		if (scrollTop > fixedPos) {
+			$("#quick").css({position : "fixed", top : 0});
+			if (scrollTop > calPos) {
+				$("#quick").css({position : "absolute", top : calPos});			
+			}
+		} else if (scrollTop < fixedPos) {	
+			$("#quick").css({position : "absolute", top : scrollSet});
+		}
+	});
+	
+});
