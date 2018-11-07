6ae4586716227feb57c67eaa9ffe7800ff9e05d6
kei5693 <keichi5693@naver.com>
2018-11-08 06:45:15 +0900

    test script

---
+++ study/animate/animate_loop/js/index.js
@@ -0,0 +1,8 @@
+﻿$(function(){
+	$.fn.topBtn = function(){
+		$.fn.baloonUp = function(){$('.topBtn').animate({'top':20},800, $.fn.baloonDown);};
+		$.fn.baloonDown = function(){$('.topBtn').animate({'top':0},800,$.fn.baloonUp);};		
+		$.fn.baloonUp();
+	}
+	$.fn.topBtn();
+});
